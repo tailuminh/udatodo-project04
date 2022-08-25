4f7e99d127d0e1f102b374eeb915934b93b1adcb
tailuminh <lutai0815@gmail.com>
2022-08-25 09:44:07 +0700

    done #Todo for .serverless.yml

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,12 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'a2p3ex15nl'
+const region = 'us-west-2'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-pw0dey3l.us.auth0.com',            // Auth0 domain
+  clientId: 'ygjM6lRK84kYtHvhJ7uYwcbgMuYjAJXw',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
